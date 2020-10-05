c6d5ae60070cb28070d86061918c60af0ace1cca
Jakub Hryc <jakubrhryc@gmail.com>
2020-10-05 20:22:02 +0200

    Create base.html

--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
-    template_name = 'pages/index.html'
+    template_name = 'pages/home.html'
 
 class HistoryPageView(TemplateView):
     template_name = 'pages/historia.html'
